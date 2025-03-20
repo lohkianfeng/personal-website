cbca5f61899ff4037e0e44e31bd52c51f249440b
lohkianfeng <lohkianfeng@gmail.com>
2025-03-20 10:22:26 +0800

    added embeddings

--- server/src/drizzle/schema.ts
+++ server/src/drizzle/schema.ts
@@ -1,8 +1,24 @@
-import { pgTable, serial, varchar, text, integer, boolean, unique, jsonb, timestamp } from "drizzle-orm/pg-core";
+import { pgTable, varchar, text, timestamp, index, vector } from "drizzle-orm/pg-core";
+import { nanoid } from "../lib/utils";
 
-export const users = pgTable("users", {
-  id: serial("id").primaryKey(),
-  sub: varchar("sub", { length: 255 }).notNull().unique(),
-  email: varchar("email", { length: 255 }).notNull().unique(),
-  created_at: timestamp("created_at", { withTimezone: true }).defaultNow().notNull(),
+export const resources = pgTable("resources", {
+  id: varchar("id", { length: 255 })
+    .primaryKey()
+    .$defaultFn(() => nanoid()),
+  content: text("content").notNull(),
+  createdAt: timestamp("created_at", { withTimezone: true }).defaultNow().notNull(),
+  updatedAt: timestamp("updated_at", { withTimezone: true }).defaultNow().notNull(),
 });
+
+export const embeddings = pgTable(
+  "embeddings",
+  {
+    id: varchar("id", { length: 255 })
+      .primaryKey()
+      .$defaultFn(() => nanoid()),
+    resourceId: varchar("resource_id", { length: 255 }).references(() => resources.id, { onDelete: "cascade" }),
+    content: text("content").notNull(),
+    embedding: vector("embedding", { dimensions: 1536 }).notNull(),
+  },
+  (table) => [index("embeddingIndex").using("hnsw", table.embedding.op("vector_cosine_ops"))]
+);
